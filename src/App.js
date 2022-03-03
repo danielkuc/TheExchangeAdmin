77506ab2c88555cfc6e19f0698dfdace56862a0c
Daniel Kuc <danpkuc@gmail.com>
2022-03-03 22:14:11 +0000

    added check if user is  authenticated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useEffect } from 'react';
 
 function App() {
-  const { isAuthenticated } = useAuth0();
-  console.log(isAuthenticated);
+  const { loginWithRedirect, getAccessTokenSilently } = useAuth0();
+
+  useEffect(() => {
+    getAccessTokenSilently().then(token => {
+      console.log("logged in");
+    }).catch(e => {
+      if (e.error === 'login_required') {
+        loginWithRedirect();
+      }
+    })
+  }, [])
   
   return (
     <div className="App">
       <NavBar/>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>Hello from admin</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
