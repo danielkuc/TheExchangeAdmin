30f2a8cbf140f8efb4aa1c134b7b88a87fed3653
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-03-03 08:00:13 +0000

    workflow test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { useAuth0 } from '@auth0/auth0-react';
 function App() {
   const { isAuthenticated, user } = useAuth0();
   console.log(isAuthenticated);
-  console.log(user);
   
   return (
     <div className="App">
