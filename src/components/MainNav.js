7926f0a73b0b80ad0c7125e407bcc98637deebdb
Daniel Kuc <danielpiotrkuc@gmail.com>
2022-03-16 08:23:54 +0000

    code cleanup, removed uneccessary code

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,16 +1,8 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 
 
 const MainNav = () => (
   <div className="navbar-nav mr-auto">
-    {/* <NavLink
-      to="https://exchange-shop.netlify.app"
-      exact="true"
-      className={({ isActive }) => "nav-link" + (isActive ? " router-link-exact-active" : "")}
-    >
-      Home
-    </NavLink> */}
     <a href="https://exchange-shop.netlify.app" className="nav-link" >Home</a>
   </div>
 );
