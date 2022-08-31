1c681d161cb0231ee3720795ca2d8593d0a81b18
Daniel Kuc <danpkuc@gmail.com>
2022-08-31 21:51:27 +0100

    testing session

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Container,Col, Row, } from 'react-bootstrap'
+import { Container,Col, Row, Button, } from 'react-bootstrap'
 import AddProductsForm from './AddProductsForm'
-//import axios from 'axios';
+import axios from 'axios';
 //import { useAuth0 } from '@auth0/auth0-react';
 
 
@@ -28,9 +28,19 @@ const Main = () => {
   //     console.log(error);
   //   }
   // }
-  
+  const requestURL = process.env.REACT_APP_SESSION_REQUEST_URL;
+  const product = {
+    "id": "62ed71b3a106298523b34519"
+  };
+
+  const makeRequest = async () => {
+    await axios.post(requestURL, product)
+    .then(response => console.log(response)).catch(error => console.log(error))
+  }
+ 
   return (
     <Container>
+      <Button variant='danger' onClick={makeRequest}  >Cart</Button>
       <Row className='vh-90 d-flex justify-content-center align-items-center' >
         <Col md={8} >
           <AddProductsForm/>        
